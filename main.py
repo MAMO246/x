a822f272575ca0df46045fa6f84ec6b5eb382824
MAMO246 <90887327+MAMO246@users.noreply.github.com>
2022-01-21 01:06:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def boten(message):
     
     A = types.InlineKeyboardButton(text ="✌️ثلاثي", callback_data="F1")
     
-    E = types.InlineKeyboardButton(text ="رباعي😗", callback_data="F2")
+    E = types.InlineKeyboardButton(text ="😗رباعي", callback_data="F2")
     
     M = types.InlineKeyboardButton('⚙️المطور', url='https://t.me/W_M_X')
     
@@ -42,7 +42,7 @@ def masg(call):
 		
 		A = types.InlineKeyboardButton(text ="✌️ثلاثي", callback_data="F1")
 
-		E = types.InlineKeyboardButton(text ="🤡رباعي", callback_data="F2")
+		E = types.InlineKeyboardButton(text ="😗رباعي", callback_data="F2")
 		
 		M = types.InlineKeyboardButton('المطور⚙️', url='https://t.me/W_M_X')
 		
