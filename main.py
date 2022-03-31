dc0f29d281e0ccb124b880d1357f2c714898a4c9
MAMO246 <90887327+MAMO246@users.noreply.github.com>
2022-03-31 19:28:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ def boten(message):
     
     mas.add(A,E,M)
     
-    bot.send_message(message.chat.id, f"- أهلاً بكً  !\n\n- بوت يوزرات تلجرام 🧑‍💻\n\n♻️ لوحة التحكم الخاصه بك ♨️",reply_markup=mas)
+    bot.send_message(message.chat.id, f"- أهلاً بكً  !\n\n- بوت تشكير يوزرات تلجرام 🧑‍💻\n\n♻️ لوحة التحكم الخاصه بك ♨️",reply_markup=mas)
     
     
 @bot.callback_query_handler(func=lambda call: True)
