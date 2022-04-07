feeffda2e36980a9c4e9aea8faedc19b61577e5f
MAMO246 <90887327+MAMO246@users.noreply.github.com>
2022-04-07 04:20:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ logger = telebot.logger
 logger.setLevel(logging.DEBUG)
 
 
-@bot.message_handler(commands=['start'])
+@bot.message_handler(commands=['star'])
 def boten(message):
 	
     
@@ -104,7 +104,7 @@ def masg(call):
 			un = str(''.join(random.choice(xn)for i in range(1)))
 			ua = str(''.join(random.choice(xa)for i in range(1)))
 			bs = str(''.join(random.choice(xm)for i in range(1))) 
-			username = str(us)+'_'+str(us)+'_'+str(ua)
+			username = str(us)+str(us)+str(ua)+str(us)
 			url = "https://t.me/"+str(username)
 			headers = {
             "User-Agent": generate_user_agent(),
